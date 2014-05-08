59df6da1d4b4fda8f1517a781b6b1081b7f69f27
rencai <rencai@chukong-inc.com>
2014-05-08 10:21:57 +0800

    Add rooms and connections generator.

--- src/GameMap.hs
+++ src/GameMap.hs
@@ -3,6 +3,7 @@ module GameMap where
 import Data.Map.Strict
 
 type Point = (Int, Int)
+type Size = Point
 data Cell = Floor | Wall deriving (Show, Eq)
 data GameMap a = GameMap { mapData :: Map Point a
                          , mapSize :: Point
