9f014f38b02b17ee50f2bbf38ea976e95c526e94
Joshua Zhang <woncomp@github.com>
2014-05-09 10:27:15 +0800

    Rewrote Console.hs with a shorter implementation ..

--- src/Console.hs
+++ src/Console.hs
@@ -1,39 +1,40 @@
-module Console where
+module Console (
+    drawGameCellMap
+    ) where
 
-import Data.Map
+import Data.Map (lookup)
 import System.Console.ANSI
 import GameMap
 
 drawStartX = 1
 drawStartY = 1
 
-gameMapToList :: GameCellMap -> [(Point, Cell)]
-gameMapToList gameCellMap = [makePair (x, y) | x <- [0..(cols-1)], y <- [0..(rows-1)]]
-    where GameMap m (cols, rows) (playerx, playery) = gameCellMap
-          makePair p = (p, getCell $ Data.Map.lookup p m)
-          getCell (Just cell) = cell
-          getCell _ = Floor
+mul :: [a] -> [b] -> [(a, b)]
+mul xs ys = [(x, y) | x <- xs, y <- ys]
 
-drawPoint :: (Point, Cell) -> IO ()
-drawPoint ((x, y), cellType) = do
-    setCursorPosition (x+drawStartX+1) (y+drawStartY+1)
-    putChar $ showANSI cellType
+gameMapToList :: GameCellMap -> [(Point, Maybe Cell)]
+gameMapToList gm@(GameMap md sz@(cols, rows) _) = map (\p -> (p, Data.Map.lookup p md)) $ mul [1..cols-1] [1..rows-1]
 
-drawBorder :: Char -> Point -> IO ()
-drawBorder c (x, y) = do
-    setCursorPosition x y
+drawChar :: Char -> Point -> IO ()
+drawChar c (x, y) = do
+    setCursorPosition y x
     putChar c
+
+drawCell :: (Point, Maybe Cell) -> IO ()
+drawCell ((x, y), Just c) = drawChar (showANSI c) (x+drawStartX, y+drawStartY)
+drawCell (p, Nothing) = drawChar ' ' p
 
 drawGameCellMap :: GameCellMap -> IO ()
 drawGameCellMap gm = 
-    let GameMap m (cols, rows) (playerx, playery) = gm
+    let GameMap m (cols, rows) _ = gm
         x0 = drawStartX
         y0 = drawStartY
         x1 = x0+cols+1
         y1 = y0+rows+1
-        drawBorder2 c p1 p2 = do drawBorder c p1 ; drawBorder c p2
+        drawBorder2 c p1 p2 = do drawChar c p1 ; drawChar c p2
     in do
-        mapM_ (\x_ -> drawBorder2 '|' (x_, y0) (x_, y1)) [x0..x1]
-        mapM_ (\y_ -> drawBorder2 '-' (x0, y_) (x1, y_)) [y0..y1]
-        mapM_ (drawBorder '+') [(x0,y0), (x1, y0), (x0, y1), (x1, y1)]
-        mapM_ drawPoint $ gameMapToList gm
+        mapM_ (\x_ -> drawBorder2 '-' (x_, y0) (x_, y1)) [x0..x1]
+        mapM_ (\y_ -> drawBorder2 '|' (x0, y_) (x1, y_)) [y0..y1]
+        mapM_ (drawChar '+') [(x0,y0), (x1, y0), (x0, y1), (x1, y1)]
+        mapM_ drawCell $ gameMapToList gm
+        setCursorPosition (y1+1) 0
